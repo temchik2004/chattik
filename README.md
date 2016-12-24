47051084f61a88d69b3733d133f0b2e06ae2c971
Artem <temchik2004@gmail.com>
2016-12-24 11:00:19 +0100

    Added sails project

---
+++ README.md
@@ -0,0 +1,3 @@
+# chattik
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/UserController.js
@@ -0,0 +1,42 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+
+  /**
+   * `UserController.login()`
+   */
+  login: function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!',
+      ert: 'asd'
+    });
+  },
+
+
+  /**
+   * `UserController.logout()`
+   */
+  logout: function (req, res) {
+    return res.json({
+      todo: 'logout() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.signup()`
+   */
+  signup: function (req, res) {
+    return res.json({
+      todo: 'signup() is not implemented yet!'
+    });
+  }
+};
+
