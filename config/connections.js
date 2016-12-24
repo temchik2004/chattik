9e50eb38c6c91434d5f89f13cb97b6a8827acda9
Artem <temchik2004@gmail.com>
2016-12-24 11:08:44 +0100

    fixed gitignore

--- config/connections.js
+++ config/connections.js
@@ -40,13 +40,14 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+  mysqlServer: {
+    adapter: 'sails-mysql',
+    host: 'localhost',
+    post: '8889',
+    user: 'sails_chattik', //optional
+    password: 'pass', //optional
+    database: 'sails_chattik' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
