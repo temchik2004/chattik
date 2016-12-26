5681570a7c7f0cf2cb861f4979df70300172ba5b
Artem <temchik2004@gmail.com>
2016-12-26 12:03:38 +0100

    Add new policy to index route

--- config/routes.js
+++ config/routes.js
@@ -33,7 +33,8 @@ module.exports.routes = {
   ***************************************************************************/
 
   '/': {
-    view: 'homepage'
+    view: 'homepage',
+    policy: 'notAuthenticated'
   },
   'get /login': {
     view: 'user/login',
